cb75645506dc3e3df8d6770f1a904d4dccaeb9d5
DominikBi <birkeldominik@gmail.com>
2019-05-01 21:36:36 +0200

    mein 1 ebstt 1 comit

--- src/main/java/team/gutterteam123/soundcontrol/gui/Line.java
+++ src/main/java/team/gutterteam123/soundcontrol/gui/Line.java
@@ -7,7 +7,8 @@ import java.awt.event.*;
 public class Line extends JComponent {
     Point mouseLocation;
     Point pointLocation;
-    public Line(){
+
+    public Line() {
         addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent mouseEvent) {
@@ -17,57 +18,19 @@ public class Line extends JComponent {
             @Override
             public void mousePressed(MouseEvent mouseEvent) {
                 super.mousePressed(mouseEvent);
-                addMouseMotionListener(new MouseMotionListener() {
-                    @Override
-                    public void mouseDragged(MouseEvent mouseEvent) {
-                        mouseLocation = mouseEvent.getPoint();
-                        pointLocation.setLocation(mouseLocation);
-                        setLocation(pointLocation);
-
-                    }
-
+                addMouseMotionListener(new MouseAdapter() {
                     @Override
                     public void mouseMoved(MouseEvent mouseEvent) {
-
+                        super.mouseDragged(mouseEvent);
                     }
                 });
             }
-
-            @Override
-            public void mouseReleased(MouseEvent mouseEvent) {
-                super.mouseReleased(mouseEvent);
-            }
-
-            @Override
-            public void mouseEntered(MouseEvent mouseEvent) {
-                super.mouseEntered(mouseEvent);
-            }
-
-            @Override
-            public void mouseExited(MouseEvent mouseEvent) {
-                super.mouseExited(mouseEvent);
-            }
-
-            @Override
-            public void mouseWheelMoved(MouseWheelEvent mouseWheelEvent) {
-                super.mouseWheelMoved(mouseWheelEvent);
-            }
-
-            @Override
-            public void mouseDragged(MouseEvent mouseEvent) {
-                super.mouseDragged(mouseEvent);
-            }
-
-            @Override
-            public void mouseMoved(MouseEvent mouseEvent) {
-                super.mouseMoved(mouseEvent);
-            }
         });
     }
+
     @Override
     public void paint(Graphics g) {
         super.paint(g);
-
 
     }
 }
